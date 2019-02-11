d8aa3b779766f9cdcc141e3a6564d3c4137a6f97
Jiwon Ashley Ryu <ryujw0108@gmail.com>
2019-02-11 02:36:46 +0000

    rendering dinner menu using Template

--- home/views.py
+++ home/views.py
@@ -6,9 +6,21 @@ def index(request):
     # print(request)
     # print(type(request))
     # print(request.META)
-    return HttpResponse('Hello, Django!')
+    # return HttpResponse(request, 'index.html')
+    return render(request, 'index.html')
     
 def dinner(request):
     box = ['치킨', '치킨', '롯데리아']
     dinner = random.choice(box)
-    return HttpResponse(dinner)
+    '''
+    render 필수인자
+    1) request, 2) template 파일(html)
+    render 선택인자
+    3) dictionary : 템플릿에서 쓸 변수 값을 정의
+    '''
+    
+    # return HttpResponse(dinner)
+    # return render(request, 'dinner.html') # dinner.html 렌더링해주기
+    return render(request, 'dinner.html', {'dinner' : dinner}) #인자 넘겨주기
+    # template는 기본적로 문법이 jinja2랑 비슷한데 장고에서는 DTL을 쓴다.
+    # dDjango Template Language
